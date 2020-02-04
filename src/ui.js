6187f9ebb4327a21f36750522bd2cb71f657ff38
Nepcam <cameron.nepe@gmail.com>
2020-02-04 20:11:06 +1300

    Add posts and show alert

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,5 @@
 class UI {
-  constructor(){
+  constructor() {
     this.post = document.querySelector('#posts');
     this.titleInput = document.querySelector('#title');
     this.bodyInput = document.querySelector('#body');
@@ -13,18 +13,58 @@ class UI {
 
     posts.forEach((post) => {
       output += `
-      <div class="card mb-3">
-        <div class="card-body">
-          <h4 class="card-title">${post.title}</h4>
-          <p class="card-text">${post.body}</p>
-          <a href="#" class="edit card-link" data-id${post.id}><i class="fa fa-pencil"></i>
-          <a href="#" class="delete card-link" data-id${post.id}><i class="fa fa-remove"></i>
+        <div class="card mb-3">
+          <div class="card-body">
+            <h4 class="card-title">${post.title}</h4>
+            <p class="card-text">${post.body}</p>
+            <a href="#" class="edit card-link" data-id="${post.id}">
+              <i class="fa fa-pencil"></i>
+            </a>
+
+            <a href="#" class="delete card-link" data-id="${post.id}">
+            <i class="fa fa-remove"></i>
+          </a>
+          </div>
         </div>
-      </div>  
       `;
     });
 
     this.post.innerHTML = output;
+  }
+
+  showAlert(message, className) {
+    this.clearAlert();
+
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.postsContainer');
+    // Get posts
+    const posts = document.querySelector('#posts');
+    // Insert alert div
+    container.insertBefore(div, posts);
+
+    // Timeout
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
   }
 }
 
